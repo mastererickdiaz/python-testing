4a42c13996002bfbd454175474fad850710dec72
Marioi Inga <mario21ic@gmail.com>
2019-07-06 13:42:55 -0500

    Agregando validadores en script

--- docker_run.sh
+++ docker_run.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-docker run node:7-alpine node --version
+dockerr run node:7-alpine node --version
+if [ $? != 0 ]; then
+  exit 1
+fi
+
 docker run node:6-alpine node --version
+if [ $? != 0 ]; then
+  exit 1
+fi
+
 docker run -v $HOME/.m2:/root/.m2 -v $WORKSPACE:/apps/ maven:3-alpine ls -la /apps
